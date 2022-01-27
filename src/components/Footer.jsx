b0975c7dbb1872a3d2e2ab540ae95f20f1f2a5cb
Fred Snyder <safe.sand1626@fastmail.com>
2022-01-27 05:26:04 +0100

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<footer>
+				<a
+					className="App-link"
+					href="https://vitejs.dev/guide/features.html"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Vite Docs
+				</a>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
