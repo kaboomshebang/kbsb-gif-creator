c35d4ecfb2aaa3c8d959065879681f8b4bb20651
Fred <fred@fastmail.nl>
2022-01-29 00:08:49 +0100

    Add TW classes to verify config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 class Header extends React.Component {
 	render() {
 		return (
-			<header>
+			<header className="container mx-auto flex flex-row justify-between">
 				<img src={this.props.url} alt={this.props.alt} />
+				<span>X</span>
 			</header>
 		);
 	}
