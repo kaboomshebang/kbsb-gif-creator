d750499581ead3d100e98b54022965e0b2595de6
Fred <fred@fastmail.nl>
2022-01-29 20:33:26 +0100

    Resize images

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -4,8 +4,8 @@ class Header extends React.Component {
 	render() {
 		return (
 			<header className="container mx-auto flex flex-row justify-between p-5">
-				<img src={this.props.urlLogo} alt={this.props.altLogo} />
-				<img src={this.props.urlMenu} alt="Menu" />
+				<img className="w-40" src={this.props.urlLogo} alt={this.props.altLogo} />
+				<img className="w-6" src={this.props.urlMenu} alt="Menu" />
 			</header>
 		);
 	}
