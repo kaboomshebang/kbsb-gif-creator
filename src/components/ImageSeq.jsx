3507c01db07a00602598bad174175f1b1c88aeae
Fred Snyder <safe.sand1626@fastmail.com>
2022-02-03 16:21:06 +0100

    Modify styling

--- src/components/ImageSeq.jsx
+++ src/components/ImageSeq.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class ImageSeq extends React.Component {
 	render() {
 		return (
-			<div id="image-container" className="grid grid-cols-2 md:grid-cols-4 gap-2 py-10">
+			<div id="image-container" className="grid grid-cols-2 md:grid-cols-4 gap-2">
 				{this.props.children}
 			</div>
 		);
