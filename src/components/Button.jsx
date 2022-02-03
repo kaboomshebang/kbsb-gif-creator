5865dab664aaabd9aff7c542afd66264f9bd01cd
Fred Snyder <safe.sand1626@fastmail.com>
2022-02-03 18:00:57 +0100

    Update button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Button extends React.Component {
 	render() {
-		return <button className="text-base font-medium bg-gray-300 px-7 py-2">{this.props.label}</button>;
+		const style = {
+			backgroundColor: this.props.color,
+		};
+
+		return (
+			<button style={style} className="text-base font-medium bg-gray-300 px-7 py-2">
+				{this.props.label}
+			</button>
+		);
 	}
 }
 
