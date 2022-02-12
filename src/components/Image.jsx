5fba72e0962aa2addda1033b3ff628da57bdacb3
Fred Snyder <safe.sand1626@fastmail.com>
2022-02-12 12:25:05 +0100

    Add logic to move the position of an image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 class Image extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = { update: 0 };
 
 		this.imgRef = React.createRef();
+		this.moveImage = this.moveImage.bind(this);
 		this.removeImage = this.removeImage.bind(this);
 	}
 
@@ -24,13 +25,41 @@ class Image extends React.Component {
 		}
 	}
 
+	// moves an image to the right
+	moveImage() {
+		// .filter filesState so that a new array is returned
+		const filesState = this.props.filesState.filter((el) => el);
+		const imgToMove = filesState[this.props.index];
+		const newPos = this.props.index + 1;
+
+		if (newPos !== filesState.length) {
+			// remove the image from the array
+			filesState.splice(this.props.index, 1);
+			// insert the image into the new position
+			filesState.splice(newPos, 0, imgToMove);
+		}
+
+		// update the files state
+		this.props.filesFunc(filesState);
+	}
+
 	removeImage() {
 		this.setState({
 			ratio: null,
 		});
+		console.log(this.props.index);
+
+		console.log(this.props.filesState);
+
 		const newFilesState = this.props.filesState.filter((el, i) => (i !== this.props.index ? el : null));
 		this.props.filesFunc(newFilesState);
+
+		console.log(this.props.index);
+		console.log(newFilesState);
+		//FIX the ratio state
 		const newRatioState = this.props.ratioState.filter((el, i) => (i !== this.props.index ? el : null));
+		console.log(newRatioState);
+
 		this.props.ratioFunc(newRatioState);
 	}
 
@@ -38,13 +67,25 @@ class Image extends React.Component {
 		return (
 			<figure className="relative flex flex-col items-center justify-between bg-gray-100">
 				{this.props.alt !== 'Placeholder' ? (
-					<button className="absolute top-2 right-2 w-6 p-1 bg-black rounded-md" onClick={this.removeImage}>
-						<img src="https://kbsb.app/assets/svg/icon_cross_white.svg" alt="Cross" />
-					</button>
+					<div className="absolute top-0 w-full flex justify-between p-2">
+						<button
+							className="w-8 p-1 bg-gray-700/75 rounded-md font-bold text-white"
+							onClick={this.moveImage}
+						>
+							{this.props.index + 1}
+						</button>
+						<button className="w-8 p-2 bg-gray-700/75 rounded-md" onClick={this.removeImage}>
+							<img
+								className="drop-shadow-md"
+								src="https://kbsb.app/assets/svg/icon_cross_white.svg"
+								alt="Cross"
+							/>
+						</button>
+					</div>
 				) : (
 					''
 				)}
-				<img ref={this.imgRef} className="max-h-60" src={this.props.url} alt={this.props.alt} />
+				<img ref={this.imgRef} className="w-full h-60 object-cover" src={this.props.url} alt={this.props.alt} />
 				<figcaption className="text-xs py-2 text-gray-400 text-center">
 					<div className="font-bold">{this.props.fileName}</div>
 
