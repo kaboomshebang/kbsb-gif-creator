d7605ebe0ba5e5f866f6c96bb0833c6d9b889111
Fred Snyder <safe.sand1626@fastmail.com>
2022-11-11 13:20:32 +0000

    Refactor to function comp, convert defaultValue to value

--- src/components/ImageSize.jsx
+++ src/components/ImageSize.jsx
@@ -1,54 +1,42 @@
-import React from 'react';
+const ImageSize = (props) => {
+	const { size = 'size', color = '#eee' } = props;
 
-class ImageSize extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleInput = this.handleInput.bind(this);
-	}
-
-	handleInput(e) {
-		if (this.props.size === 'width') {
+	const handleInput = (e) => {
+		if (props.size === 'width') {
 			// pass a new object to the setState function
 			// passing a reference does not trigger a re-render
-			this.props.sizeFunc({
+			props.sizeFunc({
 				width: e.target.valueAsNumber,
-				height: this.props.sizeState.height,
+				height: props.sizeState.height,
 			});
-		} else if (this.props.size === 'height') {
-			this.props.sizeFunc({
-				width: this.props.sizeState.width,
+		} else if (props.size === 'height') {
+			props.sizeFunc({
+				width: props.sizeState.width,
 				height: e.target.valueAsNumber,
 			});
 		}
-	}
+	};
 
-	render() {
-		const style = {
-			backgroundColor: this.props.color,
-		};
+	const style = {
+		backgroundColor: color,
+	};
 
-		return (
-			<div className="py-2">
-				<span className="inline-block w-16 text-left">{this.props.size}</span>
-				<input
-					style={style}
-					type="number"
-					className="w-20 bg-gray-200 p-2"
-					min="0"
-					max="9999"
-					step="1"
-					defaultValue={this.props.defValue}
-					onChange={this.handleInput}
-				/>
-				<span className="pl-2">pixels</span>
-			</div>
-		);
-	}
-}
-
-ImageSize.defaultProps = {
-	size: 'size',
-	color: '#eee',
+	return (
+		<div className="py-2">
+			<span className="inline-block w-16 text-left">{size}</span>
+			<input
+				style={style}
+				type="number"
+				className="w-20 bg-gray-200 p-2"
+				min="0"
+				max="9999"
+				step="1"
+				value={props.value}
+				onChange={handleInput}
+			/>
+			<span className="pl-2">pixels</span>
+		</div>
+	);
 };
 
 export default ImageSize;
